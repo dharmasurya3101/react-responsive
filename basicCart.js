39a0fb3c9387a1585b788c17defc85e7dab5b24a
dharmasurya3101 <58599226+dharmasurya3101@users.noreply.github.com>
2022-03-19 18:49:04 +0530

    Add files via upload
    created with props

--- basicCart.js
+++ basicCart.js
@@ -3,43 +3,13 @@ import  '../style.css'
 
 export default function BasicCart(props) {
   return (
-    <div className='height'>
-        
-      <div className='container'>
-        <div className='row'>
-        <div className='col-md-3 left'>
-        <div className='box left'>
-       
-        <img  className='image left' src={props.img}/>
-        <h4>{props.name}</h4>
-       </div>
-       </div>
-       <div className='col-md-3 left'>
-        <div className='box left'>
-       
-        <img  className='image left' src={props.img}/>
-        <h4>{props.name}</h4>
-       </div>
-       </div>
-       <div className='col-md-3 left'>
-        <div className='box left'>
-       
-        <img  className='image left' src={props.img}/>
-        <h4>{props.name}</h4>
-       </div>
-       </div>
-       <div className='col-md-3 left'>
-        <div className='box left'>
-       
-        <img  className='image left' src={props.img}/>
-        <br />
-        <br />
-        <h4>{props.name}</h4>
-       </div>
-       </div>
-        </div>
-        </div>
-        
+    <div className='box'>
+      <div className='.left'>
+      <img src={ props.path}/>
+      <h4>{props.name}</h4>
+      <p>{props.price}</p>
+      <button className='button'>Buy Now</button>
+      </div>
     </div>
   )
 }
